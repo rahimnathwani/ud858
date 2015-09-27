d4bfcb61c5d1dcdd734f22b0a63136de5c4db496
Rahim Nathwani <rahim@encona.com>
2015-09-27 11:31:00 +0800

    Import Session and Wish into conference.py

--- ConferenceCentral_Complete/conference.py
+++ ConferenceCentral_Complete/conference.py
@@ -36,6 +36,12 @@ from models import ConferenceForms
 from models import ConferenceQueryForm
 from models import ConferenceQueryForms
 from models import TeeShirtSize
+from models import Session
+from models import SessionForm
+from models import SessionForms
+from models import Wish
+from models import WishForm
+from models import WishForms
 
 from settings import WEB_CLIENT_ID
 from settings import ANDROID_CLIENT_ID
